3463968c62bf1a9b201fd6d335c24e9bfe4fb0d3
Ben Hall <benhall2121@yahoo.com>
2011-06-24 22:01:19 -0700

    benhall2121@yahoo.com adding the files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swchd::Application.config.secret_token = '31c1a0aca4e0587c97b2bfd246f088184259bee2d35b9cba08824e3acfdba973b1c0a08526663879321230f36d3dd5802d322cc0fc487917868870a38d4ff270'
