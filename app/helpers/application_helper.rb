732ac2a23018e626d49424d1876c079aabe629e6
Ben Hall <benhall2121@yahoo.com>
2011-06-26 12:13:12 -0700

    benhall2121@yahoo.com route changes cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
     return @carrot.goal.finished	  
   end
   
+  def already_friends(friend_id)
+  	  if Friendship.find_by_user_id_and_friend_id(current_user.id, friend_id)
+  	  	  return true
+  	  else
+  	  	  return false
+  	  end
+  end
+  
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+  
+  has_many :goals, :foreign_key => "user_id"
 end
